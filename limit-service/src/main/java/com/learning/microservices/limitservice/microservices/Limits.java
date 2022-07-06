2ddebb3a1cdd32be21c1058dfbff6544cce55e1b
saram <saram@organicresponse.com>
2022-07-06 09:33:51 +0200

    initial commit

---
+++ limit-service/src/main/java/com/learning/microservices/limitservice/microservices/Limits.java
@@ -0,0 +1,31 @@
+package com.learning.microservices.limitservice.microservices;
+
+public class Limits {
+
+    private int minimum;
+    private int maximum;
+
+    public Limits(int minimum, int maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+
+    public Limits() {
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+}
